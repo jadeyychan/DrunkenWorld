8049f9edddb1964e062012561700d4810bcac271
Jade <jadeyychan@gmail.com>
2016-11-16 11:38:02 -0500

    slider play animate

--- js/slider.js
+++ js/slider.js
@@ -1,11 +1,15 @@
+var min_year = 1960;
+var max_year = 2016;
+var default_year = 1990;
+
 // Initialize slider
 var slider = d3.slider()
-				.min(1960)
-				.max(2015)
-				.tickValues(d3.range(1960, 2016, 10))
-				.stepValues(d3.range(1960, 2016, 1))
+				.min(min_year)
+				.max(max_year)
+				.tickValues(d3.range(min_year, max_year, 10))
+				.stepValues(d3.range(min_year, max_year, 1))
 				.showRange(true)
-				.value(1990)
+				.value(default_year)
                 .tickFormat(d3.format("d"))
                 .callback(function(event) {
                     year = String(self.slider.value());
@@ -20,6 +24,21 @@ var slider = d3.slider()
                     });
                 });
 
-
 // Render the slider in the div
 d3.select('.slider').call(slider);
+
+
+// Animate button
+$('.slider-button').click(
+    function() {
+        time_travel(min_year);
+    });
+
+function time_travel(pos) {
+    setTimeout(function() {
+        slider.setValue(pos);
+        if (pos < max_year) {
+            time_travel(++pos);
+       }
+   }, 80);
+}

2ae774a6908f6e2968d8ba76f7354ccd386409db
Jade <jadeyychan@gmail.com>
2016-12-04 15:11:51 -0500

    fixed page transitions, and about page, and logo in top navbar

--- js/page.js
+++ js/page.js
@@ -1,53 +1,16 @@
-var CURRENT_PAGE = "landing-page";
-
 function show_viz() {
 	$(".viz-page").removeClass("hidden");
 	$(".landing-page").slideUp("slow");
-	CURRENT_PAGE = "viz-page";
 }
 
 function show_landing_page() {
-	$(".landing-page").slideDown("slow");
-	CURRENT_PAGE = "landing-page";
+	$(".landing-page").slideDown("slow", function() {
+		$(".viz-page").addClass("hidden");
+		$(".info-page").addClass("hidden");
+	});
 }
 
 function show_help() {
-	if ($(".info-page").hasClass("hidden")) {
-
-		/* show_info_page */
-		$(".info-page").removeClass("hidden");
-		$("."+CURRENT_PAGE).slideUp("slow");
-
-		/* Buttons */
-		$(".x-button").removeClass("hidden");
-		$(".i-button").addClass("hidden");
-
-		// remove z- index
-		$(".info-page").css("z-index", "0");
-
-	} else {
-
-		$("."+CURRENT_PAGE).slideDown("slow", function() {
-			$(".info-page").addClass("hidden");
-		});
-
-		// current page remove hidden
-		$("."+CURRENT_PAGE).removeClass("hidden");
-
-		/* Buttons */
-		$(".x-button").addClass("hidden");
-		$(".i-button").removeClass("hidden");
-	}
+	$(".info-page").removeClass("hidden");
+	$(".landing-page").slideUp("slow");
 }
-
-
-/*
-landing page 5
-
-info!
-
-viz page
-
-info
-
-*/

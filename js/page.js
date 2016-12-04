11ea8d896c352f1f1a15f8615b310ab6c92319aa
Jade <jadeyychan@gmail.com>
2016-12-04 15:41:58 -0500

    styling changes

--- js/page.js
+++ js/page.js
@@ -1,13 +1,13 @@
-function show_viz() {
-	$(".viz-page").removeClass("hidden");
-	$(".landing-page").slideUp("slow");
-}
-
 function show_landing_page() {
 	$(".landing-page").slideDown("slow", function() {
 		$(".viz-page").addClass("hidden");
 		$(".info-page").addClass("hidden");
 	});
+}
+
+function show_viz() {
+	$(".viz-page").removeClass("hidden");
+	$(".landing-page").slideUp("slow");
 }
 
 function show_help() {

492dde8808acf69731a03195215f73d6a64fc5b0
Jade <jadeyychan@gmail.com>
2016-12-13 20:55:37 -0500

    page slides down when video is done

--- js/info-page-slide.js
+++ js/info-page-slide.js
@@ -1,10 +1,12 @@
+/********** Page Slide Animation *************/
 
+// Slides page down
 function info_page_slide() {
     $('html,body').animate({
         scrollTop: $(".data").offset().top -80}, 'slow');
-    $(".see-more-button").addClass("hidden");
 }
 
+// shows and hides the button
 $(window).scroll(function() {
     var height = $(window).scrollTop();
 
@@ -14,3 +16,25 @@ $(window).scroll(function() {
     	$(".see-more-button").addClass("hidden");
     }
 });
+
+/********** Embedded Youtube Video *************/
+
+// create youtube player
+var player;
+function onYouTubePlayerAPIReady() {
+    player = new YT.Player('info-video-player', {
+      height: '315',
+      width: '560',
+      videoId: 'O-eeiWZ7WRY',
+      events: {
+        'onStateChange': onPlayerStateChange
+      }
+    });
+}
+
+// when video ends
+function onPlayerStateChange(event) {        
+    if(event.data === 0) {          
+        info_page_slide();
+    }
+}
